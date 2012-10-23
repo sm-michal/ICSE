25a642e17f29092db56f3154f04a965bf3839deb
Michal <smolo@localhost.localdomain>
2012-10-23 18:42:12 +0200

    #1 menu główne atrapa

--- src/pl/smolo/icse/view/MainWindow.java
+++ src/pl/smolo/icse/view/MainWindow.java
@@ -1,7 +1,13 @@
 package pl.smolo.icse.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame
@@ -41,6 +47,19 @@ public class MainWindow extends JFrame
 	 */
 	private JMenuBar menuBar;
 	
+	private JMenu startMenu;
+	private JMenuItem newSearchMenuItem;
+	private JMenuItem historyMenuItem;
+	private JMenuItem quitMenuItem;
+	
+	private JMenu sitesMenu;
+	private JCheckBoxMenuItem mobileEuMenuItem;
+	private JCheckBoxMenuItem autotraderPlMenuItem;
+	
+	private JMenu helpMenu;
+	private JMenuItem helpMenuItem;
+	private JMenuItem aboutMenuItem;
+	
 	/**
 	 * Wypisane wszystkie opcje wyszukiwania, kazda moze byc osobno usunieta
 	 * Stronicowanie
@@ -48,6 +67,8 @@ public class MainWindow extends JFrame
 	 * lista z wynikami
 	 */
 	private JPanel resultsPanel;
+	
+	private MainWindowActionListener actionListener;
 	
 	public MainWindow()
 	{
@@ -57,9 +78,65 @@ public class MainWindow extends JFrame
 	
 	private void initComponents()
 	{
+		actionListener = new MainWindowActionListener();
+		
 		this.setSize(1200, 750);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		menuBar = new JMenuBar();
 		
+		startMenu = new JMenu("Start");
+		newSearchMenuItem = new JMenuItem("Nowe wyszukiwanie");
+		newSearchMenuItem.addActionListener(actionListener);
+		
+		historyMenuItem = new JMenuItem("Poprzednie wyszukiwania");
+		historyMenuItem.addActionListener(actionListener);
+		
+		quitMenuItem = new JMenuItem("Wyjście");
+		quitMenuItem.addActionListener(actionListener);
+		
+		startMenu.add(newSearchMenuItem);
+		startMenu.add(historyMenuItem);
+		startMenu.addSeparator();
+		startMenu.add(quitMenuItem);
+		
+		sitesMenu = new JMenu("Portale");
+		mobileEuMenuItem = new JCheckBoxMenuItem("mobile.eu");
+		mobileEuMenuItem.addActionListener(actionListener);
+		
+		autotraderPlMenuItem = new JCheckBoxMenuItem("autotrader.pl");
+		autotraderPlMenuItem.addActionListener(actionListener);
+		
+		sitesMenu.add(mobileEuMenuItem);
+		sitesMenu.add(autotraderPlMenuItem);
+		
+		helpMenu = new JMenu("Pomoc");
+		helpMenuItem = new JMenuItem("Pomoc");
+		helpMenuItem.addActionListener(actionListener);
+		
+		aboutMenuItem = new JMenuItem("O programie");
+		aboutMenuItem.addActionListener(actionListener);
+		
+		helpMenu.add(helpMenuItem);
+		helpMenu.add(aboutMenuItem);
+		
+		menuBar.add(startMenu);
+		menuBar.add(sitesMenu);
+		menuBar.add(helpMenu);
+		
+		this.setJMenuBar(menuBar);
+		
+		this.setVisible(true);
+	}
+	
+	private class MainWindowActionListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent evt)
+		{
+			if (quitMenuItem.equals(evt.getSource()))
+				System.exit(0);
+		}
 	}
 }
